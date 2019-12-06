50e0439eaa4a43b100e85e42e59f67ba6eb914ba
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-06 11:58:03 -0500

    filled out palyer card

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+class PlayerCard extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            players: []
+        };
+    }
+
+    componentDidMount() {
+        console.log('render mounting');
+        axios
+        .get('http://localhost:5000/api/players')
+        .then (res => this.setState({ players: res.data }))
+        .catch(err =>  console.log(err))
+    }
+
+
+    render() {
+        return (
+            
+            <div>
+      
+            {this.state.players.map(player => 
+            
+            <div className="card1">
+        
+            <h2>Name: {player.name[3]}</h2>
+            <h2>Country: {player.country}</h2>
+            <h2>Times Searched: {player.searches}</h2> 
+        
+            </div>
+            
+            )}
+        
+            </div>   
+        )
+    }
+
+
+
+
+
+}
