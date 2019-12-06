ae02ea6a583d2f5c22117f9562571307a7ee07fe
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-06 12:17:18 -0500

    adding testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import PlayerCard from './components/PlayerCard';
+import NavbarDarkM from './components/NavbarDarkM'
+import {render} from '@testing-library/react';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('navbar darkmode', () => {
+  const nav = render(<NavbarDarkM />)
+  console.log(nav);
+})
+
+test('player card ', () => {
+  const card = render(<PlayerCard />)
+  console.log(card);
+})
+
+
